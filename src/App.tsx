163c0e64608d4a3c73966fc6f05fca4579229dd6
Micheal Arsenault <mike2ykme@gmail.com>
2022-05-02 23:52:05 -0500

    CP-2 | Adding basic SetChoice options

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {SetChoice} from "./SetChoice/SetChoice";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="/choice" element={<SetChoice/>}/>
+                <Route path="/choice2" element={<SetChoice/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
