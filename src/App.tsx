ed9f510f06f7dab92867345300ec5e135449edf2
oxytocin <>
2022-05-05 18:47:47 -0400

    Bootstrapped login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Link, Route, Routes } from "react-router-dom";
+import { Button, Form } from 'react-bootstrap';
 
 function App() {
     return (
@@ -17,10 +18,12 @@ function Login() {
     return (
         <>
             <header className="Login-header">Flashcard Bonanza</header>
-            <input id="username-field" type="text" placeholder="Username" className="Login-login-input"></input>
-            <input id="password-field" type="text" placeholder="Password" className="Login-login-input"></input>
-            <button className="Login-button">Log in</button>
-            <p className="Login-small-text">New user? <Link to="/signup">Sign up</Link></p>
+            <Form>
+                <Form.Control type="text" placeholder="Username" />
+                <Form.Group className="mb-1"><Form.Control type="password" placeholder="Password" /></Form.Group>
+                <Button variant="dark" size="sm" href="#">Log in</Button>
+                <p className="Login-small-text">New user? <Link to="/signup">Sign up</Link></p>
+            </Form>
         </>
   );
 }
