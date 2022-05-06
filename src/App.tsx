6db59891981fe260507f3cf4cca6933d859d442e
oxytocin <>
2022-05-06 15:19:31 -0400

    Add profile dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+import { Navbar, NavDropdown } from 'react-bootstrap';
 import Login from "./Login/Login"
 import CreateOrJoin from "./CreateOrJoin/CreateOrJoin";
 import Signup from "./Signup/Signup";
+import account from "./images/account.svg";
 
 function App() {
     return (
         <div className="App">
+            <Navbar bg="light" fixed="top" expand="lg">
+                <NavDropdown className="ms-auto" title={ <img src={account} /> }>
+                    <NavDropdown.Item>Log out</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar>
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
