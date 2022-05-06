f334837042f996b0701939b5bd00ca5292ed467d
Micheal Arsenault <mike2ykme@gmail.com>
2022-05-05 20:04:05 -0500

    CP-2 | Added additional login page that would work with oauth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Link, Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
 
 function App() {
     return (
         <div className="App">
             <Routes>
+                {/*<Route path="/" element={<Login />} />*/}
                 <Route path="/" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
             </Routes>
@@ -13,12 +15,12 @@ function App() {
     );
 }
 
-function Login() {
+function LoginManual() {
     return (
         <>
             <header className="Login-header">Flashcard Bonanza</header>
             <input id="username-field" type="text" placeholder="Username" className="Login-login-input"></input>
-            <input id="password-field" type="text" placeholder="Password" className="Login-login-input"></input>
+            <input id="password-field" type="password" placeholder="Password" className="Login-login-input"></input>
             <button className="Login-button">Log in</button>
             <p className="Login-small-text">New user? <Link to="/signup">Sign up</Link></p>
         </>
