af0b6ad8e13cbe437507887a5cf80cf8b161b307
Micheal Arsenault <mike2ykme@gmail.com>
2022-05-06 22:32:45 -0500

    Added additional navigation changes as well as a mobile version for smaller screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from "react-router-dom";
-import { Navbar, NavDropdown } from 'react-bootstrap';
+import {Route, Routes} from "react-router-dom";
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import Login from "./Login/Login"
 import CreateOrJoin from "./CreateOrJoin/CreateOrJoin";
 import Signup from "./Signup/Signup";
 import account from "./images/account.svg";
+import {HeaderNav} from "./Header/Navigation";
+
 
 function App() {
     return (
         <div className="App">
-            <Navbar bg="light" fixed="top" expand="lg">
-                <NavDropdown className="ms-auto" title={ <img src={account} /> }>
-                    <NavDropdown.Item>Log out</NavDropdown.Item>
-                </NavDropdown>
-            </Navbar>
+            <HeaderNav/>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/create-or-join" element={<CreateOrJoin />} />
+                <Route path="/" element={<Login/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="/create-or-join" element={<CreateOrJoin/>}/>
             </Routes>
         </div>
     );
