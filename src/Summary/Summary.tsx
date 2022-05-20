9541d73cfc11753ab42ea389f96f52fe264cb07b
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-05-20 10:09:12 -0500

    Add summary page

---
+++ src/Summary/Summary.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+
+function ScoreSummary() {
+
+    return(
+    <Container>
+        This is a test
+    </Container>
+    );
+}
+
+export default ScoreSummary;
