75031f849a9a3ab5cb3268531cf361078a046f05
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-05-27 21:42:47 -0500

    initial tags commit

--- src/CreateOrJoin/CreateOrJoin.tsx
+++ src/CreateOrJoin/CreateOrJoin.tsx
@@ -4,11 +4,11 @@ import { Button, ButtonToolbar } from 'react-bootstrap';
 function CreateOrJoin() {
     return (
         <>
-            <header className="CreateOrJoinHeader">Flashcard Bonanza</header>
-            <p>Welcome, [user]! What would you like to do?</p>
+            <header data-cy="header" className="CreateOrJoinHeader">Flashcard Bonanza</header>
+            <p data-cy="greeting">Welcome, [user]! What would you like to do?</p>
             <ButtonToolbar>
-                <Button variant="dark" size="lg" className="mx-1">Host room</Button>
-                <Button variant="dark" size="lg" className="mx-1">Join room</Button>
+                <Button data-cy="host-btn" variant="dark" size="lg" className="mx-1">Host room</Button>
+                <Button data-cy="join-btn" variant="dark" size="lg" className="mx-1">Join room</Button>
             </ButtonToolbar>
         </>
     );
--- src/Header/Navigation.tsx
+++ src/Header/Navigation.tsx
@@ -6,17 +6,17 @@ export const HeaderNav: React.FC = () => (
     <Navbar bg="light" expand="md" fixed="top">
         <Container>
             {/* This is the mobile element, will do a 'hamburger' button */}
-            <Navbar.Brand href="#home">Flashcard Bonanza</Navbar.Brand>
+            <Navbar.Brand data-cy="nav-home" href="#home">Flashcard Bonanza</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse data-cy="nav-collapse" id="basic-navbar-nav">
                 <Nav className="d-block d-sm-block d-md-none">
-                    <Nav.Link href="#home">Log Out</Nav.Link>
+                    <Nav.Link data-cy="nav-logout" href="#home">Log Out</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
             {/* this is the original design, but not meant for mobile */}
             <Nav className="d-none d-md-block">
-                <NavDropdown title={<img src={account}/>} id="basic-nav-dropdown" className="d-flex">
-                    <NavDropdown.Item href="#">Log Out</NavDropdown.Item>
+                <NavDropdown data-cy="nav-dropdown" title={<img src={account}/>} id="basic-nav-dropdown" className="d-flex">
+                    <NavDropdown.Item data-cy="dropdown-logout" href="#">Log Out</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
         </Container>
