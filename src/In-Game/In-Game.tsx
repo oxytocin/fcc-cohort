a1bafe1f555e73c02544322c6243356fe3505e49
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-06-02 00:09:51 -0500

    Cycles questions correctly

--- src/In-Game/In-Game.tsx
+++ src/In-Game/In-Game.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
-import {ProgressBar, Container, Card, Row, Col, Spinner, Stack, ToggleButton} from 'react-bootstrap';
+import React, {useEffect, useState, useRef} from 'react';
+import {ProgressBar, Container, Card, Row, Col, Spinner, Stack, ToggleButton, Button} from 'react-bootstrap';
 import Countdown from 'react-countdown';
 import data from './in-game-data';
 
 // A way to shuffle the answers
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array: {}[]) {
-    let currentIndex = data.Answers.length,  randomIndex;
+    let currentIndex = array.length,  randomIndex;
     // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
@@ -18,28 +18,52 @@ function shuffle(array: {}[]) {
     };
     return array
 }
-shuffle(data.Answers);
 
-// Date.now's must be outside of component function so they don't re-render and restart on each state update
-const userTime = Date.now() + 10000; // <--- this number to be determined by host
-const bufferTime = userTime + 5000; // default buffer time between questions
+shuffle(data.Cards);
+for (let i = 0; i < data.Cards.length; i++) {
+    shuffle(data.Cards[i].Answers)
+}
+
+const timeLimit = 1000 * 10 // second number to be chosen by host in room, pass into here
 
 function InGame() {
-
+    const [card, setCard] = useState(0) // card is the question index of the shuffled deck
+    const [userTime, setUserTime] = useState(Date.now() + timeLimit)
     const alphabet = ["A","B","C","D"]; // for assigning answer card labels
-    const correctAns = data.Answers.map(item => item.IsCorrect);
+    const correctAns = data.Cards[card].Answers.map(item => item.IsCorrect);
     const ansHeader = document.getElementsByClassName("Answer-card-header");
+    const bufferTime = userTime + 5000; // default buffer time between questions
+
+    useEffect(()=> {
+        const timeIsUp = setTimeout(()=> {
+            setDisabledState(true);
+            checkAns();
+            displayCorrect(ansHeader);
+            setIndicatorState("outline-dark");
+        }, timeLimit);
+        return () => clearTimeout(timeIsUp)
+    }, [bufferTime]); // if userTime changes, this effect will fire again-- it's a good thing
+
+    useEffect(()=> {
+        const next = setTimeout(()=> {
+            if (card < data.Cards.length - 1) {
+                setCard(card+1)
+                setCheckedState( new Array(data.Cards[card+1].Answers.length).fill(false) )               
+                setDisabledState(false)
+                setUserTime(Date.now() + timeLimit)
+                setIndicatorState("outline-secondary")
+                unmarkCorrect(ansHeader)
+            } else {
+                return null // game ends here
+            }
+        }, timeLimit+5000);
+        return () => clearTimeout(next)
+    }, [bufferTime])
 
     // TIMER FUNCTIONS //
     // Renderer callback with condition
     const timer = ({seconds, completed }: {seconds:number, completed:boolean}) => {
       if (completed) {
-        // Render answer results and do the following //////////// STATE SHOULD NOT BE UPDATED UNDER THIS CHILD COMPONENT, NOR SHOULD IT BE UPDATED UNDER A CONDITION. but idk how exactly to do it otherwise.
-        checkAns();
-        setDisabledState(true);
-        displayCorrect(ansHeader);
-        setIndicatorState("outline-dark");
-        //addPoints; some way to add a point for a correct answer
         return <Completionist />;
       } else {
         // Render the Question countdown
@@ -54,21 +78,26 @@ function InGame() {
     const Completionist = () => <div><Countdown date={bufferTime} renderer={buffer}/></div>;
     const buffer = ({seconds, completed }: {seconds:number, completed:boolean}) => {
         if (completed) {
-                        ////////////// LOAD NEXT QUESTION HERE???
-            return <span>next question load</span>;
+            
+            return <Button size="lg" variant="success" className="rounded-pill">Go To Summary</Button>;
         } else {
-            return <span>Next Question in...{seconds}</span>;
+            if (card === data.Cards.length - 1) {
+                return <span><Spinner animation="border" variant="light" className="mx-2"/></span>
+            } else {
+                return <span>Next Question: {seconds}</span>;
+            }
         }
     };
 
     // evaluates user's input against IsCorrect answer data
     const checkAns = () => {
         for (let i = 0; i < correctAns.length; i++) {
-            if (!correctAns[i] === (checkedState[i])) {
+            if (!correctAns[i] === (checkedRef.current[i])) {
                 console.log("incorrect")
                 return false
             }
         }
+        setUserScore(userScore + 1)
         console.log("correct!")
         return true
     };
@@ -83,11 +112,20 @@ function InGame() {
             }
         };
     };
+    const unmarkCorrect = (elements: HTMLCollection) => {
+        for (let i = 0; i < elements.length; i++) {
+            const elementClass = elements[i].classList;
+            elementClass.add("bg-light");
+            elementClass.remove("bg-success");
+        };
+    };
 
-//  const [userScore, setUserScore] = useState(0);
+    const [userScore, setUserScore] = useState(0);
     const [disabledState, setDisabledState] = useState(false);
     const [indicatorState, setIndicatorState] = useState("outline-secondary");
-    const [checkedState, setCheckedState] = useState( new Array(data.Answers.length).fill(false) );
+    const [checkedState, setCheckedState] = useState( new Array(data.Cards[card].Answers.length).fill(false) );
+    const checkedRef = useRef(checkedState) // must use Ref to relay updated information to the setTimeout function
+    checkedRef.current = checkedState
 
 //// https://www.freecodecamp.org/news/how-to-work-with-multiple-checkboxes-in-react/
     const handleOnChange = (position: number) => {
@@ -97,36 +135,37 @@ function InGame() {
         setCheckedState(updatedCheckedState);
     }
 
-    console.log("actual ans: " + correctAns);   // logs the IsCorrect Values provided by the Answer array (for verification only)
-    console.log("checked: " + checkedState);    // logs the user's selected answer choices (for verification)
-//  console.log("userScore: " + userScore)      // logs the user's score (for verification)
+//  console.log("checkedRef: " + checkedRef.current) // logs selected answer choices passed as references for use in setTimeout
+//  console.log("actual ans: " + correctAns);   // logs the IsCorrect Values provided by the Answer array (for verification only)
+//  console.log("checked: " + checkedState);    // logs the user's selected answer choices (for verification)
+//  console.log("user Score: " + userScore)      // logs the user's score (for verification)
 
     return (
     <div className="pt-5">
         <Container fluid="md">
             <Row className="mt-md-5 pt-5" > {/* Progress and Timer */}
                 <Col md={4}>
-                    <ProgressBar animated variant="dark" now={(3/15)*100} /> {/* replace 3/15 with {QuesNumber}/{deck.length} */}
-                    <h3 className="text-start">Question {3} of {15}</h3>
+                    <ProgressBar data-cy="progress" animated variant="dark" now={( (card+1) / data.Cards.length)*100} /> {/* {QuesNumber}/{deck.length} */}
+                    <h3 data-cy="ques-num" className="text-start">Question {card+1} of {data.Cards.length}</h3>
+                    <h4 data-cy="score" className="text-start">Score: {userScore}</h4>
                     </Col>
                 <Col md={{span:3, offset:5}}>
                     <h3 className="rounded rounded-pill bg-dark text-white">
-                        <Countdown date={userTime} renderer={timer}/>
+                        <Countdown data-cy="timer" key={card} date={userTime} renderer={timer}/>
                     </h3>
                 </Col>
             </Row>
             <Row> {/* Question Data */}
-                <h1 className="col-md-8 offset-md-2 text-break word-break">
-                {data.Question}
+                <h1 data-cy="question" className="col-md-8 offset-md-2 text-break word-break">
+                {data.Cards[card].Question}
                 </h1>
             </Row>
             {/* Answer Choices Data */}
-            <Row md={2} className="mt-md-4">
-                {data.Answers.map((item, i) => {
+            <Row data-cy="answers" md={2} className="mt-md-4">
+                {data.Cards[card].Answers.map((item, i) => {
                     return(
                     <Col xs={12}>
                         <ToggleButton id={alphabet[i]} type="checkbox" className="w-100"
-                        key={i}
                         variant={indicatorState}
                         value={item.Id}
                         checked={checkedState[i]}
--- src/In-Game/in-game-data.tsx
+++ src/In-Game/in-game-data.tsx
@@ -25,11 +25,43 @@ const sampleAnswers = [{
     IsCorrect:true
   }];
 
+// Sample Question
+const card = {
+  Id: 0,
+  Question: "this is the sample data question 1?",
+  Answers: [{
+        Id: 0,
+        Name: "name1correct",
+        Value: "value2correct",
+        IsCorrect: true,
+        FlashCardId: 0
+        }, {
+        Id: 0,
+        Name: "name2correct",
+        Value: "value2correct",
+        IsCorrect: true,
+        FlashCardId: 0
+        }, {
+        Id:0,
+        Name: "name3false",
+        Value: "value3false",
+        IsCorrect: false,
+        FlashCardId: 0
+        }, {
+        Id:0,
+        Name: "name4false",
+        Value: "value4false",
+        IsCorrect: false,
+        FlashCardId: 0
+        }
+    ]
+}
 
-// Question-answer data model
+// Deck data model
 const data = {
+  Cards: [{
     Id: 0,
-    Question: "this is the sample data question?",
+    Question: "this is the sample data question 1?",
     Answers: [{
           Id: 0,
           Name: "name1correct",
@@ -56,5 +88,93 @@ const data = {
           FlashCardId: 0
           }
       ]
-  }
+  },{
+    Id: 0,
+    Question: "this is the sample data question 2?",
+    Answers: [{
+          Id: 0,
+          Name: "name1correct",
+          Value: "value2correct",
+          IsCorrect: true,
+          FlashCardId: 0
+          }, {
+          Id: 0,
+          Name: "name2correct",
+          Value: "value2correct",
+          IsCorrect: true,
+          FlashCardId: 0
+          }, {
+          Id:0,
+          Name: "name3false",
+          Value: "value3false",
+          IsCorrect: false,
+          FlashCardId: 0
+          }, {
+          Id:0,
+          Name: "name4false",
+          Value: "value4false",
+          IsCorrect: false,
+          FlashCardId: 0
+          }
+      ]
+  },{
+    Id: 0,
+    Question: "this is the sample data question 3?",
+    Answers: [{
+          Id: 0,
+          Name: "name1correct",
+          Value: "value2correct",
+          IsCorrect: true,
+          FlashCardId: 0
+          }, {
+          Id: 0,
+          Name: "name2correct",
+          Value: "value2correct",
+          IsCorrect: true,
+          FlashCardId: 0
+          }, {
+          Id:0,
+          Name: "name3false",
+          Value: "value3false",
+          IsCorrect: false,
+          FlashCardId: 0
+          }, {
+          Id:0,
+          Name: "name4false",
+          Value: "value4false",
+          IsCorrect: false,
+          FlashCardId: 0
+          }
+      ]
+  },{
+    Id: 0,
+    Question: "this is the sample data question 4?",
+    Answers: [{
+          Id: 0,
+          Name: "name1correct",
+          Value: "value2correct",
+          IsCorrect: true,
+          FlashCardId: 0
+          }, {
+          Id: 0,
+          Name: "name2correct",
+          Value: "value2correct",
+          IsCorrect: true,
+          FlashCardId: 0
+          }, {
+          Id:0,
+          Name: "name3false",
+          Value: "value3false",
+          IsCorrect: false,
+          FlashCardId: 0
+          }, {
+          Id:0,
+          Name: "name4false",
+          Value: "value4false",
+          IsCorrect: false,
+          FlashCardId: 0
+          }
+      ]
+  }]
+}
   export default data
