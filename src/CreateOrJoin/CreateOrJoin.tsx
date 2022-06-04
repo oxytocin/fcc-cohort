66ea343465114878506117425373741513bd171f
Micheal Arsenault <mike2ykme@gmail.com>
2022-06-03 23:32:08 -0500

    updating configs and how the login page works by checking for token

--- src/CreateOrJoin/CreateOrJoin.tsx
+++ src/CreateOrJoin/CreateOrJoin.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import {Button, ButtonToolbar} from 'react-bootstrap';
+import jwtDecode from "jwt-decode";
+import {bonanza_token} from "../Constants";
 
 function CreateOrJoin() {
+    let token = localStorage.getItem(bonanza_token) ?? ""
+
+    const decoded:{"firstName":string} = jwtDecode(token)
+    let user = decoded.firstName
+
     return (
         <>
             <header data-cy="header" className="CreateOrJoinHeader">Flashcard Bonanza</header>
-            <p data-cy="greeting">Welcome, [user]! What would you like to do?</p>
+            <p data-cy="greeting">Welcome, {user}! What would you like to do?</p>
             <ButtonToolbar data-cy="btn-toolbar">
                 <Button data-cy="host-btn" variant="dark" size="lg" className="mx-1">Host room</Button>
                 <Button data-cy="join-btn" variant="dark" size="lg" className="mx-1">Join room</Button>
--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import {config} from "../Constants"
+import {bonanza_token, config} from "../Constants"
 
 // Curl version of how to use the bearer. basically you'll just need to add this to a header
 // curl localhost:8088/restricted -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0NjE5NTcxMzZ9.RB3arc4-OyzASAaUhC2W3ReWaXAt_z2Fd3BN4aWTgEY"
 // so just add the header 'Authorization' and then use the value 'Bearer <token>'
 const Login = () => {
+    const localToken = localStorage.getItem(bonanza_token)
+    if (localToken != undefined || localToken !== ""){
+        window.location.href = config.CREATE_OR_JOIN
+    }
+
     const connectClick = () => {
-        const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=849784468632-n9upp7q0umm82uecp5h3pfdervht7sjg.apps.googleusercontent.com&redirect_uri=${config.OAUTH_REDIRECT_URL}&response_type=code&scope=openid profile email`;
-        // rather than open a popup, we redirect to Google's authentication
-        // That way, the token can be saved and will persist in
-        // localStorage (see OauthRedirect.tsx)
+        const url = `${config.REDIRECT_URL}?client_id=${config.CLIENT_ID}` +
+            `&redirect_uri=${config.OAUTH_FRONTEND_REDIRECT_URL}&response_type=${config.RESPONSE_TYPE}`
         window.location.href = url
     }
     return <button data-cy="connect" onClick={connectClick}>Connect</button>
