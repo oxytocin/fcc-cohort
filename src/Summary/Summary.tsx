2ebadf36b0b62c53667a449491c86106932179ee
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-06-06 14:05:16 -0500

    Summary is responsive

--- src/Summary/Summary.tsx
+++ src/Summary/Summary.tsx
@@ -1,55 +1,44 @@
 import React, {Fragment} from 'react';
 import {Container, Table, Button} from 'react-bootstrap';
-
-  // score would be determined as: {user_correct_responses}/{deck.length} //
+import {config} from '../Constants'
+import {testData} from '../In-Game/in-game-data'
 
 function ScoreSummary() {
+  const rankedScores = testData.allScores.sort((a, b) => b.score - a.score)
+  const numQuestions = testData.numQuestions
 
-  const testData = [
-    {user:"Guy1", score: 10},
-    {user:"Guy4", score: 14},
-    {user:"Guy2", score: 11},
-    {user:"Guy3", score:12}
-  ].sort((a, b) => b.score - a.score)
+  const clickReturn = () => {
+    const url = config.CREATE_OR_JOIN
+    window.location.href = url
+  }
 
   return(
-  <Container fluid className="bg-dark min-vh-100">
-    <div className="position-absolute top-50 start-50 translate-middle p-5 bg-gradient border border-2 rounded">
-        <h1 data-cy="summary" className="text-white mb-3">Score Summary</h1>
+  <Container fluid className="bg-dark p-3 border border-2 rounded">
 
-        <Table data-cy="table" striped className="table table-light" style={{width:500}}>
-          <thead>
-            <tr data-cy="table-headers">
-              <th data-cy="user" scope="col">User</th>
-              <th data-cy="score" scope="col" colSpan={2}>Score</th>
-              <th data-cy="rank" scope="col">Rank</th>
-            </tr>
-          </thead>
-
-          <tbody data-cy="table-body">
-              {testData.map((item, i) => {
-                  return (
-                        <Fragment>
-                        <tr>
-                            <th scope="row">{item.user}</th>
-                            <td colSpan={2}>{item.score} / {/*deck.length, example 15*/}15</td>
-                            <td>{i+1}</td>
-                        </tr>
-                    </Fragment>
-                  );
-                })}
-            </tbody>
-          <tfoot>
-              <tr>
-                <th data-cy="host" scope="row">HostName</th>
-                <td data-cy="host-score" colSpan={2}>15{/*hostScore*/} / 15{/*deck.length*/}</td>
-                <td data-cy="host-blank">--</td>
-              </tr>
-          </tfoot>
-        </Table>
-
-      <Button data-cy="return-btn" variant="success" size="lg" className="mx-1">Return to Room</Button>
-    </div>
+    <h1 data-cy="summary" className="text-white mb-3">Score Summary</h1>
+    <Table data-cy="table" size="sm" striped hover responsive="sm" className="table table-secondary w-75 mx-auto">
+      <thead>
+        <tr data-cy="table-headers">
+          <th data-cy="user" scope="col">User</th>
+          <th data-cy="score" scope="col">Score</th>
+          <th data-cy="rank" scope="col">Rank</th>
+        </tr>
+      </thead>
+      <tbody data-cy="table-body">
+        {rankedScores.map((item, i) => {
+            return (
+              <Fragment>
+                <tr>
+                  <th scope="row">{item.user}</th>
+                  <td>{item.score} / {numQuestions}</td>
+                  <td>{i+1}</td>
+                </tr>
+              </Fragment>
+            );
+          })}
+        </tbody>
+    </Table>
+    <Button data-cy="return-btn" variant="success" className="mx-1" onClick={clickReturn}>Return</Button>
   </Container>
   );
 }
