49ef021316693372b232001c63565d549d50d1ec
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-06-08 17:48:44 -0500

    Update room_spec tests

--- cypress/integration/4-Room/room_spec.js
+++ cypress/integration/4-Room/room_spec.js
@@ -4,24 +4,30 @@
 // this needs a lot of attention
 
 describe('Cannot access if not signed in', ()=> {
-    it('Throws 404 if not signed in', ()=> {
-        cy.intercept('/some-room-id').as('shouldFail')
-        cy.visit('/some-room-id')
+    it.skip('Throws 404 if not signed in', ()=> {
+        cy.intercept('/waiting-room') // need to mock created room id?
+        cy.visit('/waiting-room')
 
         cy.wait('@shouldFail').its('response.statusCode').should('eq', 404)
         expect('this test').to.be('failing') // fix pathname
     })
 })
 
-describe('Checks Room functionality', ()=> {
+describe('Check Waiting Room functionality', ()=> {
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.loginByGoogleApi()
+        cy.visit('/waiting-room')
     })
 
     it('Displays all elements', ()=> {
         cy.contains('Room Code').should('be.visible');
-        cy.contains('form').should('be.visible')
+        cy.get('#inlineFormInput').should('be.visible').and('have.attr', 'readonly')
+        cy.get('.input-group-text').find('input').should('be.visible').and('have.attr', 'type', 'image')
+
+        cy.get('.input-group-text').find('input').focus().realClick()
+        cy.get('#button-tooltip').should('be.visible').and('have.text', "Copied!")
+
         cy.contains('Users Joined').should('be.visible')
     })
 
