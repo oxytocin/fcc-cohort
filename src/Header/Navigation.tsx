8f093f73befd62629b395cf9ac375609603a4739
oxytocin <>
2022-06-09 10:37:29 -0400

    Basic logout

--- src/Header/Navigation.tsx
+++ src/Header/Navigation.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import { useLocation } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import account from "../images/account.svg";
 
 export const HeaderNav: React.FC = () => {
     // Don't show navbar on login page (root)
+    const navigate = useNavigate();
     if (useLocation().pathname === "/") return null;
+    function logOut() {
+        // TODO: doesn't invalidate the token, so if an attacker got it or
+        // something, they could still log in.
+        localStorage.clear();
+        navigate("/");
+    }
     return (
         <Navbar bg="light" expand="md" fixed="top">
             <Container data-cy="nav-container">
@@ -20,7 +27,7 @@ export const HeaderNav: React.FC = () => {
                 {/* this is the original design, but not meant for mobile */}
                 <Nav className="d-none d-md-block">
                     <NavDropdown data-cy="nav-dropdown" title={<img alt="account-icon" src={account}/>} id="basic-nav-dropdown" className="d-flex">
-                        <NavDropdown.Item data-cy="dropdown-logout" href="#">Log Out</NavDropdown.Item>
+                        <NavDropdown.Item onClick={logOut} data-cy="dropdown-logout" href="#">Log Out</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Container>
