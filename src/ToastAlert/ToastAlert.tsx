e450b905f9a99de9a2a05350778cbf31be133250
oxytocin <>
2022-06-24 07:30:33 -0400

    Prettier toasts (thanks, Andrew)

--- src/ToastAlert/ToastAlert.tsx
+++ src/ToastAlert/ToastAlert.tsx
@@ -1,19 +1,23 @@
 import {useContext} from "react";
-import {Toast} from "react-bootstrap";
+import {Toast, ToastContainer} from "react-bootstrap";
 import {ToastContext} from "../App";
 
 export function ToastAlert() {
     const toastContext = useContext(ToastContext);
     return (
-        <Toast show={toastContext.toastShow} onClose={() => {
-                toastContext.setToastShow(false);
-            }
-        }>
-            <Toast.Header>
-                <strong className="me-auto">Alert</strong>
-            </Toast.Header>
-            <Toast.Body>{toastContext.toastText}</Toast.Body>
-        </Toast>
+        <ToastContainer position="bottom-center">
+            <Toast bg="secondary" show={toastContext.toastShow} onClose={() => {
+                    toastContext.setToastShow(false);
+                }
+            }>
+                <Toast.Header>
+                    <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                    <strong className="me-auto">Bonanza!</strong>
+                    <small>Notification</small>
+                </Toast.Header>
+                <Toast.Body className="text-white">{toastContext.toastText}</Toast.Body>
+            </Toast>
+        </ToastContainer>
     )
 }
 
